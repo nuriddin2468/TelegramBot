1a4e482ae07e7d903b2ef73080342d13963dfd4b
Nuriddin <nuriddin_2468@mail.ru>
2019-07-15 10:18:41 +0500

    Update Working.py

--- MelekTelegramBot/Working.py
+++ MelekTelegramBot/Working.py
@@ -1,11 +1,11 @@
 import telebot
 from telebot.types import Message
 
-TOKEN = "400408075:AAHhDxhhmoAMFrSajhS8QQfQPQdL2keL3q8"
+TOKEN = "836997197:AAFvCr4oePuitIZJ6-cKLXNufSfquho6nz8"
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(func=lambda message: True, commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, Бот работает пока-что) Тебе повезло!')
 
